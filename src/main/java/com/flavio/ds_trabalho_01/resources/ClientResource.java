7c19c6b1f836983f21d131d81179762c2f7201f1
Flávio Gonçalves <flaviojgfilho@hotmail.com>
2021-05-14 15:09:05 -0300

    Client Resource

---
+++ src/main/java/com/flavio/ds_trabalho_01/resources/ClientResource.java
@@ -0,0 +1,10 @@
+package com.flavio.ds_trabalho_01.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/clients")
+public class ClientResource {
+
+}
