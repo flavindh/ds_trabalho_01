69650688ef4fbe77f3d047ef8d8d22e75caec331
Flávio Gonçalves <flaviojgfilho@hotmail.com>
2021-05-14 14:55:56 -0300

    Project created

---
+++ src/main/java/com/flavio/ds_trabalho_01/DsTrabalho01Application.java
@@ -0,0 +1,13 @@
+package com.flavio.ds_trabalho_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsTrabalho01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsTrabalho01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/flavio/ds_trabalho_01/DsTrabalho01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flavio.ds_trabalho_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsTrabalho01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
